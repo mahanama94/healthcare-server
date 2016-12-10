4c453b4ad7aa9ed3b4151a8eef407f280ff452f8
mahanama94 <bhanuka.14@cse.mrt.ac.lk>
2016-12-10 14:52:28 +0530

    Code improvements
    Added Medical officerDAO

---
+++ src/main/java/lk/bhanuka/DAO/MedicalOfficerDAO.java
@@ -0,0 +1,26 @@
+package lk.bhanuka.DAO;
+
+import lk.bhanuka.models.MedicalOfficer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by bhanuka on 12/10/16.
+ */
+public class MedicalOfficerDAO extends DAO{
+
+    public ArrayList getMedicalOfficers(){
+        // Perform database operations with this.dataservice
+        return null;
+    }
+
+    public MedicalOfficer getMedicalOfficer(){
+        // Perform database operations with this.dataservice
+        return new MedicalOfficer();
+    }
+
+    public MedicalOfficer addMedicalOfficer(MedicalOfficer medicalOfficer){
+        // Perform database operations with this.dataservice
+        return null;
+    }
+}
