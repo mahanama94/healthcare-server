9cd592608f2eb46fc5fd079233e319a892a7c40f
ThejanW <thejanwijesinghe.14@cse.mrt.ac.lk>
2016-12-13 23:04:55 +0530

    SQL recent changes: no person with the same nic, defined get_age function

--- epidemic.sql
+++ epidemic.sql
@@ -1,54 +1,60 @@
 CREATE TABLE district(
-  district_id VARCHAR(5),
+  district_id int NOT NULL AUTO_INCREMENT,
   district_name VARCHAR(20) NOT NULL,
-  PRIMARY KEY(district_id)
+  PRIMARY KEY(district_id),
+  UNIQUE (district_name)
 );
 CREATE TABLE province(
-  province_id VARCHAR(5),
+  province_id int NOT NULL AUTO_INCREMENT,
   province_name VARCHAR(20) NOT NULL,
-  PRIMARY KEY(province_id)
+  PRIMARY KEY(province_id),
+  UNIQUE (province_name)
 );
 CREATE TABLE disease(
-  disease_id VARCHAR(5),
+  disease_id int NOT NULL AUTO_INCREMENT,
   disease_name VARCHAR(10) NOT NULL,
   description VARCHAR(20) NOT NULL,
   treatment VARCHAR(20) NOT NULL,
-  PRIMARY KEY(disease_id)
+  PRIMARY KEY(disease_id),
+  UNIQUE (disease_name)
 );
 CREATE TABLE patient(
-  patient_id VARCHAR(5),
+  patient_id int NOT NULL AUTO_INCREMENT,
   nic VARCHAR(10),
   NAME VARCHAR(20),
   dob DATE,
-  district_id VARCHAR(5),
+  district_id int,
   PRIMARY KEY(patient_id),
-  FOREIGN KEY(district_id) REFERENCES district(district_id) ON DELETE SET NULL
+  FOREIGN KEY(district_id) REFERENCES district(district_id) ON DELETE SET NULL,
+  UNIQUE (nic)
 );
 CREATE TABLE medical_officer(
-  med_officer_id VARCHAR(5),
+  med_officer_id int NOT NULL AUTO_INCREMENT,
   nic VARCHAR(10) NOT NULL,
   NAME VARCHAR(20) NOT NULL,
   dob DATE NOT NULL,
-  district_id VARCHAR(5),
+  district_id int,
   specialization VARCHAR(10) NOT NULL,
   PRIMARY KEY(med_officer_id),
-  FOREIGN KEY(district_id) REFERENCES district(district_id) ON DELETE SET NULL
+  FOREIGN KEY(district_id) REFERENCES district(district_id) ON DELETE SET NULL,
+  UNIQUE (nic)
 );
 CREATE TABLE health_officer(
-  health_officer_id VARCHAR(5),
+  health_officer_id int NOT NULL AUTO_INCREMENT,
   nic VARCHAR(10) NOT NULL,
   NAME VARCHAR(20) NOT NULL,
   dob DATE NOT NULL,
-  province_id VARCHAR(5),
+  province_id int,
   specialization VARCHAR(10) NOT NULL,
   PRIMARY KEY(health_officer_id),
-  FOREIGN KEY(province_id) REFERENCES province(province_id) ON DELETE SET NULL
+  FOREIGN KEY(province_id) REFERENCES province(province_id) ON DELETE SET NULL,
+  UNIQUE (nic)
 );
 CREATE TABLE med_report(
   report_date DATE NOT NULL,
-  patient_id VARCHAR(5) NOT NULL,
-  disease_id VARCHAR(5) NOT NULL,
-  med_officer_id VARCHAR(5) NOT NULL,
+  patient_id int NOT NULL,
+  disease_id int NOT NULL,
+  med_officer_id int NOT NULL,
   PRIMARY KEY(
     patient_id,
     disease_id,
@@ -59,11 +65,40 @@ CREATE TABLE med_report(
   FOREIGN KEY(med_officer_id) REFERENCES medical_officer(med_officer_id) ON DELETE CASCADE
 );
 CREATE TABLE med_officer_assign(
-  med_officer_id VARCHAR(5),
-  health_officer_id VARCHAR(5) NOT NULL,
+  med_officer_id int NOT NULL,
+  health_officer_id int NOT NULL,
   PRIMARY KEY(
     med_officer_id
   ),
   FOREIGN KEY(med_officer_id) REFERENCES medical_officer(med_officer_id) ON DELETE CASCADE,
   FOREIGN KEY(health_officer_id) REFERENCES health_officer(health_officer_id) ON DELETE CASCADE
 );
+
+
+CREATE FUNCTION `get_age`(
+  `date_of_birth` DATE,
+  `current_time` DATETIME
+) RETURNS INT(11) UNSIGNED COMMENT 'Age Calculation' DETERMINISTIC NO SQL SQL SECURITY DEFINER RETURN(
+  (
+    YEAR(CURRENT_TIME) - YEAR(date_of_birth)
+  ) -(
+    (
+      DATE_FORMAT(CURRENT_TIME, '00-%m-%d') < DATE_FORMAT(date_of_birth, '00-%m-%d')
+    )
+  )
+);
+
+
+
+/*
+
+To use the get_age function:
+
+SELECT get_age(date_of_birth, NOW()) AS age FROM person WHERE id = 1
+
+
+To add a insert to a auto increment table:
+
+INSERT INTO disease VALUES(NULL,'desease1','discription1','treatment1');
+
+*/
