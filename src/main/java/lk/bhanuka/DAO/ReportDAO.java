97bc0241b95871796d21967f1bc5738bdc14fd5c
ThejanW <thejanwijesinghe.14@cse.mrt.ac.lk>
2016-12-22 00:30:31 +0530

    Report Insert added

--- src/main/java/lk/bhanuka/DAO/ReportDAO.java
+++ src/main/java/lk/bhanuka/DAO/ReportDAO.java
@@ -15,103 +15,116 @@ import org.springframework.objenesis.instantiator.basic.NewInstanceInstantiator;
  */
 public class ReportDAO extends DAO {
 
-    public ReportDAO(){
-        super();
-        PatientDAO patientDAO = new PatientDAO();
-        this.tableName = "med_report";
-        
-    }
+	public ReportDAO() {
+		super();
+		PatientDAO patientDAO = new PatientDAO();
+		this.tableName = "med_report";
 
-    public List<Report> getReports(){
+	}
 
-        return this.findReports(new ArrayList<String>());
+	public List<Report> getReports() {
 
-    }
-    
-//    public Report getReport(String med_officer_nic, String patient_nic){
-//    	ArrayList<String> conditions = new ArrayList<String>();
-//    	
-//    	conditions.add(" med_officer_nic " + med_officer_nic);
-//    	conditions.add(" patient_nic " + patient_nic);
-//    	
-//    	
-//    }
+		return this.findReports(new ArrayList<String>());
 
-//     TODO modify with new primary keys
-//    public ArrayList<E>Report getReport(ArrayList conditions){
-//
-//        //ArrayList<String> conditions = new ArrayList<String>();
-//
-//        //conditions.add(" id = "+ id.toString());
-//
-//        List<HashMap> results = this.dataService.get(this.tableName, conditions);
-//
-//        for(HashMap element : results){
-//
-//            //MedicalOfficer medicalOfficer = (new MedicalOfficerDAO()).getMedicalOfficer(Long.valueOf(element.get("medicalOfficerId").toString()));
-//
-//            
-//        	
-//        	//Report report = new Report(element.get("id").toString(), medicalOfficer);
-//
-//        	Report report = new Report();
-//        	
-//        	report.setLast_updated("last_updated");
-//            report.setComments("comments");
-//            report.setMed_officer_nic("med_officer_nic");
-//            report.setMed_officer_name("med_officer_name");
-//            report.setDisease_name("disease_name");
-//            report.setPatient_nic("patient_nic");
-//            report.setPatient_name("patient_name");
-//            report.setPrescription("prescription");
-//            report.setReport_date("report_date");
-//        	
-//            return report;
-//
-//        }
-//
-//        return null;
-//    }
+	}
 
-    public List<Report> findReports(ArrayList<String> conditions){
+	// public Report getReport(String med_officer_nic, String patient_nic){
+	// ArrayList<String> conditions = new ArrayList<String>();
+	//
+	// conditions.add(" med_officer_nic " + med_officer_nic);
+	// conditions.add(" patient_nic " + patient_nic);
+	//
+	//
+	// }
 
-        ArrayList<Report> reports = new ArrayList<Report>();
+	// TODO modify with new primary keys
+	// public ArrayList<E>Report getReport(ArrayList conditions){
+	//
+	// //ArrayList<String> conditions = new ArrayList<String>();
+	//
+	// //conditions.add(" id = "+ id.toString());
+	//
+	// List<HashMap> results = this.dataService.get(this.tableName, conditions);
+	//
+	// for(HashMap element : results){
+	//
+	// //MedicalOfficer medicalOfficer = (new
+	// MedicalOfficerDAO()).getMedicalOfficer(Long.valueOf(element.get("medicalOfficerId").toString()));
+	//
+	//
+	//
+	// //Report report = new Report(element.get("id").toString(),
+	// medicalOfficer);
+	//
+	// Report report = new Report();
+	//
+	// report.setLast_updated("last_updated");
+	// report.setComments("comments");
+	// report.setMed_officer_nic("med_officer_nic");
+	// report.setMed_officer_name("med_officer_name");
+	// report.setDisease_name("disease_name");
+	// report.setPatient_nic("patient_nic");
+	// report.setPatient_name("patient_name");
+	// report.setPrescription("prescription");
+	// report.setReport_date("report_date");
+	//
+	// return report;
+	//
+	// }
+	//
+	// return null;
+	// }
 
-        List<HashMap> results = this.dataService.get(this.tableName, conditions);
-        System.out.println(results);
-        
-        for(HashMap element : results){
-        	
-        	Report report = new Report(); 
-        	
-        	//report.setReport_date((String)element.get("report_date"));
-        	report.setPatient_nic((String)element.get("patient_nic"));
-        	report.setMed_officer_nic((String)element.get("med_officer_nic"));
-        	report.setDisease_id((Long)element.get("disease_id"));
-        	report.setComments((String)element.get("comments"));
-        	report.setPrescription((String)element.get("prescriptions"));
-        	//report.setLast_updated((String)element.get("last_updated"));
-        	
-        	
-            reports.add(report);
+	public List<Report> findReports(ArrayList<String> conditions) {
 
-        }
+		ArrayList<Report> reports = new ArrayList<Report>();
 
-        return reports;
-    }
+		List<HashMap> results = this.dataService.get(this.tableName, conditions);
+		System.out.println(results);
 
-    // TODO implement add method
-    public Report addReport(Report report){
-    	
-    	
-        // Perform database operations with this.dataService
-    	
+		for (HashMap element : results) {
 
-        return report;
+			Report report = new Report();
 
-    }
-    
-    public HashMap updateReport(Report report){
+			// report.setReport_date((String)element.get("report_date"));
+			report.setPatient_nic((String) element.get("patient_nic"));
+			report.setMed_officer_nic((String) element.get("med_officer_nic"));
+			report.setDisease_id((Long) element.get("disease_id"));
+			report.setComments((String) element.get("comments"));
+			report.setPrescription((String) element.get("prescriptions"));
+			// report.setLast_updated((String)element.get("last_updated"));
+
+			reports.add(report);
+
+		}
+
+		return reports;
+	}
+
+	public HashMap addReport(Report report) {
+
+		HashMap values = new HashMap();
+
+		values.put("patient_nic", report.getPatient_nic());
+		values.put("disease_id", report.getDisease_id());
+		values.put("med_officer_nic", report.getMed_officer_nic());
+
+		HashMap response = this.dataService.insert(this.tableName, values);
+
+		HashMap results = new HashMap();
+
+		if (response.get("error") == null) {
+
+			return response;
+
+		} else {
+			results.put("error", response.get("error"));
+			return results;
+		}
+
+	}
+
+	public HashMap updateReport(Report report) {
 
 		HashMap values = new HashMap();
 		values.put("comments", report.getComments());
@@ -119,7 +132,7 @@ public class ReportDAO extends DAO {
 		values.put("last_updated_by", report.getLast_updated_by());
 
 		ArrayList conditions = new ArrayList();
-		conditions.add("patient_nic" + " = '"+ report.getPatient_nic() + "'");
+		conditions.add("patient_nic" + " = '" + report.getPatient_nic() + "'");
 		conditions.add("med_officer_nic" + " = '" + report.getMed_officer_nic() + "'");
 		conditions.add("disease_id" + " = '" + report.getDisease_id() + "'");
 
@@ -132,25 +145,15 @@ public class ReportDAO extends DAO {
 
 		System.out.println(response.toString());
 
-		if(response.get("error") == null){
-
-//			response.put(this.primaryKey, disease.getId());
-//			response.put(this.name, disease.getName());
-			
-			//results.put("disease", this.createDisease(response));
+		if (response.get("error") == null) {
 			return response;
-
 		}
 
 		else {
-
 			results.put("error", response.get("error"));
-
+			return results;
 		}
 
-		return results;
 	}
-    
-    
-    
+
 }
--- src/main/java/lk/bhanuka/controller/ReportController.java
+++ src/main/java/lk/bhanuka/controller/ReportController.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletRequest;
  * Created by bhanuka on 12/10/16.
  */
 @RestController
-public class ReportController extends Controller{
+public class ReportController extends Controller {
 
 	private ReportDAO reportDAO;
 
@@ -35,22 +35,22 @@ public class ReportController extends Controller{
 
 		if (role == "patient") {
 			ArrayList conditions = new ArrayList();
-	
+
 			conditions.add("patient_nic" + " = '" + nic + "'");
-	
+
 			return reportDAO.findReports(conditions);
 		} else {
-	
+
 			return null;
 		}
 	}
 
 	@RequestMapping(value = "/reports/search", method = RequestMethod.GET)
 	public List findReports(HttpServletRequest request) {
-		
+
 		String role = Auth.getUser().getRole();
-		
-		if (role == "medical_officer" || role == "health_officer" || role == "patient"){
+
+		if (role == "medical_officer" || role == "health_officer" || role == "patient") {
 			HashMap params = new HashMap();
 
 			params.put("name", request.getParameter("name"));
@@ -58,69 +58,60 @@ public class ReportController extends Controller{
 			ArrayList conditions = new ArrayList();
 			String name = (String) params.get("name");
 
-
 			conditions.add("patient_nic" + " in " + "(select nic from patient where NAME like '%" + name + "%')");
 			return reportDAO.findReports(conditions);
-			
-		}
-		else{
+
+		} else {
 			return null;
 		}
 
 	}
-	
-// TODO: 
-	
-//	@RequestMapping(value = "/reports", method = RequestMethod.POST)
-//	public HashMap addDisease(HttpServletRequest request) {
-//		
-//		ArrayList<String> required = new ArrayList<String>();
-//		
-//		required.add("disease_id");
-//        required.add("med_officer_nic");	
-//        required.add("patient_nic");
-//
-//		HashMap validated = this.validator.validate(request, required);
-//		
-//		if (validated.get("error") == null) {
-//			Report report = new Report();
-//			report.
-//			
-//
-//			Disease newDisease = new Disease(0,
-//					request.getParameter("name"), request.getParameter("description"),
-//					request.getParameter("treatment"),
-//					request.getParameter("symptoms"));
-//
-//				return this.diseaseDAO.addDisease(newDisease);
-//		}
-//		return validated;
-//	}
+
+
+	@RequestMapping(value = "/reports", method = RequestMethod.POST)
+	public HashMap addDisease(HttpServletRequest request) {
+
+		ArrayList<String> required = new ArrayList<String>();
+
+		required.add("disease_id");
+		required.add("patient_nic");
+
+		String med_officer_nic = Auth.getUser().getNic();
+
+		HashMap validated = this.validator.validate(request, required);
+
+		if (validated.get("error") == null) {
+			Report report = new Report();
+			report.setPatient_nic(request.getParameter("patient_nic"));
+			report.setDisease_id(Long.parseLong(request.getParameter("disease_id")));
+			report.setMed_officer_nic(med_officer_nic);
+			return this.reportDAO.addReport(report);
+		}
+		return validated;
+	}
 
 	@RequestMapping(value = "/reports", method = RequestMethod.PUT)
 	public HashMap updateDisease(HttpServletRequest request) {
-		
+
 		String patient_nic = request.getParameter("patient_nic");
 		String med_officer_nic = request.getParameter("med_officer_nic");
 		Long disease_id = Long.parseLong(request.getParameter("disease_id"));
 		String last_updated_by = Auth.getUser().getNic();
-		
+
 		String comments = request.getParameter("comments");
 		String prescription = request.getParameter("prescription");
-		
+
 		Report report = new Report();
-		
+
 		report.setPatient_nic(patient_nic);
 		report.setMed_officer_nic(med_officer_nic);
 		report.setComments(comments);
 		report.setPrescription(prescription);
 		report.setDisease_id(disease_id);
 		report.setLast_updated_by(last_updated_by);
-		
+
 		return reportDAO.updateReport(report);
-		
+
 	}
-
-
 
 }
